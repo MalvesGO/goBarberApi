2a1a7d6690002baaca835ac123969601f050538b
Marcelo Alves <marceloalves_ac@hotmail.com>
2019-10-23 10:44:55 -0300

    config nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,14 @@
+// https://mailtrap.io
+
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 465,
+  secure: false,
+  auth: {
+    user: '86f19edcdbf647',
+    pass: 'fabb454f27f66c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
